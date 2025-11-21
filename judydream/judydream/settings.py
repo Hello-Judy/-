fbddf0e057a398c5460e81fea200ff703834d5e7
yuw821 <yuw821@lehigh.edu>
2025-11-21 04:40:35 -0600

    11/21 目前已能简单爬取单个全职信息以及对应时间与链接，后续可继续完善爬取更多信息字段 以及增加翻页功能

--- judydream/judydream/settings.py
+++ judydream/judydream/settings.py
@@ -85,3 +85,4 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
--- judydream/judydream/spiders/a51jobspider.py
+++ judydream/judydream/spiders/a51jobspider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import re
 
 class A51jobspiderSpider(scrapy.Spider):
     # 每个项目的唯一名称，来区分不同的Spider
@@ -18,11 +18,13 @@ class A51jobspiderSpider(scrapy.Spider):
     #  该方法负责解析返回的数据(response data)，提取数据(生成item)以及生成需要进一步处理的URL的 Request 对象
     def parse(self, response):
         if "searchintern" in response.url:
-            pass
+            self.parse_intern(response)
         elif "haigui" in response.url:
-            pass
+            self.parse_haigui(response)
+
         elif "%E6%95%B0%E6%8D%AE%E8%BF%90%E8%90%A5" in response.url:
-            pass
+            # 把子解析函数产生的 Request 交回给引擎
+            yield from self.parse_fulltime(response)
 
     def parse_intern(self, response):
         print("This is intern page.")
@@ -32,10 +34,43 @@ class A51jobspiderSpider(scrapy.Spider):
         print("This is haigui page.")
         pass
 
+
+    
+
     def parse_fulltime(self, response):
         print("This is fulltime page.")
-        second_page_link = response.css('#list .search-list-href a::attr(href)').getall()
-        print(f"How many {len(second_page_link)} elements found on the page.!!!")
+        get_link = response.css('#list .search-list-href[data-v-73d24bfa]::attr(href)').getall()
+        print("type of link:", type(get_link))
+        print(f"在一级页面找到 {len(get_link)} 个职位链接")
+        # 遍历一级页面的每一个职位的链接
+        for link in get_link:
+            # 处理链接，去掉参数部分
+            #link = link.split('?')[0]  
+            link = response.urljoin(link)
+            print("Processed second URL:", link)
+
+            # 创建二级页面的请求，并指定回调函数为 parse_job_detail
+            yield scrapy.Request(url=link, 
+                                 callback=self.parse_job_detail
+                                )  
+            
+    def parse_job_detail(self, response):
+        item = {}
+
+        item['job_title'] = response.css('#detail .jobtitle .detail-title-left-top .job[data-v-1bc90c70]::text').get()
+        item['job_description'] = response.css('#detail .detail-content-common .text[data-v-1bc90c70]:last-child::text').get()
+        item['company_name'] = response.css('#detail .detail-content-compnav-center span[data-v-1bc90c70]::text').get()
+        item['posting_date'] = response.css('#detail .jobtitle .detail-title-left-center .time[data-v-1bc90c70]::text').get().strip()
+        pattern = r"^\D+"
+        new_posting_date = re.sub(pattern, "", item['posting_date'])
+        item['posting_date'] = new_posting_date
+        item["Type"] = "Full-time"
+        item['application_link'] = response.url
+        
+        print("Application Link:", item['application_link'])
 
 
 
+
+    
+  
